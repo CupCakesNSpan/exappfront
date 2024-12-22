c1feb1773da469b40ba5dd22c1a85dd95d643e39
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-12-22 14:38:04 +0900

    feat: define colors and import fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./styles.css";
 import Link from 'next/link';
-
-
 
 export const metadata: Metadata = {
   title: "Exercise App",
@@ -16,12 +15,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Quattrocento:wght@400;700&display=swap" rel="stylesheet" />
+      </head>
       <body>
         <header>
-          <nav>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/login">Login</Link>
+          <nav className="nav-styles">
+            <div className="nav-title">
+              AppNameAndLogo
+            </div>
+            <div className="nav-navlinks">
+              <div className="nav-navlink">
+                <Link href="/">Home</Link>
+              </div>
+              <div className="nav-navlink">
+                <Link href="/about">About</Link>
+              </div>
+              <div className="nav-navlink">
+                <Link href="/about">Workouts</Link>
+              </div>
+            </div>
+            <div className="nav-loginlink">
+              <div className="nav-navlink">
+                <Link href="/login">Login</Link>
+              </div>
+            </div>
           </nav>
         </header>
         <main>
