bb2f38bcea87f47b863cff0e7e42f4ea4b45fc3c
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-12-22 21:15:28 +0900

    style: add home page styling

---
+++ app/credits/page.tsx
@@ -0,0 +1,11 @@
+
+const CreditsPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Credits</h1>
+      <p>Photo by <a href="https://unsplash.com/@jonathanborba?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Jonathan Borba</a> on <a href="https://unsplash.com/photos/woman-exercising-indoors-lrQPTQs7nQQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a> </p>
+    </div>
+  );
+}
+
+export default CreditsPage;
