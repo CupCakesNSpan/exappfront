fe36cbc0013f0b2383e963dad974ce99b35b658f
ai-n0808 <aichan.88.aichan@gmail.com>
2025-02-13 17:17:51 +0900

    feat: adding styling file for register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+import styles from "./RegisterPage.module.css";
 
 const Register = () => {
-    return (
-        <div>This is the registration page</div>
-    );
+  return (
+    <div className={styles.registerPage}>
+      <div className={styles.titleSection}>
+        <h1>Register</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
