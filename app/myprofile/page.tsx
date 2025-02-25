0bae4c10fcf08c4cfe9373b91a517db47fc1ce6a
ai-n0808 <aichan.88.aichan@gmail.com>
2025-02-25 14:16:43 +0900

    feat: Displaying user name

--- app/myprofile/page.tsx
+++ app/myprofile/page.tsx
@@ -1,13 +1,16 @@
+"use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useContext } from "react";
+import { useAuth } from "../context/AuthContext";
 import styles from "./MyProfile.module.css";
 
 const MyProfile = () => {
+  const { user } = useAuth();
   const router = useRouter();
   return (
     <div>
-      <p>Welcome</p>
+      <p>Welcome {user?.displayName}</p>
     </div>
   );
 };
