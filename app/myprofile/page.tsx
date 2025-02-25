6bb0072ac14699adce7c522e0958300fbc517eb8
ai-n0808 <aichan.88.aichan@gmail.com>
2025-02-25 17:10:04 +0900

    feat: basic panel for onboarding

--- app/myprofile/page.tsx
+++ app/myprofile/page.tsx
@@ -9,8 +9,23 @@ const MyProfile = () => {
   const { user } = useAuth();
   const router = useRouter();
   return (
-    <div>
-      <p>Welcome {user?.displayName}</p>
+    <div className={styles.myProfilePage}>
+      <div className={styles.usernameSection}>
+        <p>Welcome {user?.displayName}</p>
+      </div>
+      <div className={styles.onboardingButtonSection}>
+        <button type="button" className={styles.onboardingButton}>
+          Complete onboarding
+        </button>
+      </div>
+      <div className={styles.onboardingSection}>
+        <button>Your Exercise Now</button>
+        <button>Barriers</button>
+        <button>Motivation</button>
+        <button>Become an exerciser</button>
+        <button>Goals</button>
+        <button>Contract</button>
+      </div>
     </div>
   );
 };
